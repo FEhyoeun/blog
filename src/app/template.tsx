d9d3581d66cfe4251718bfdda252a85f65e2eba6
FEhyoeun <khyoeun92@gmail.com>
2025-07-03 17:25:00 +0900

    feat: framer-motion 전체 페이지에 적용 * next의 template을 이용해서 해당 모션을 전체적으로 적용

---
+++ src/app/template.tsx
@@ -0,0 +1,6 @@
+import { ReactNode } from 'react';
+import PageTransition from '@/components/page-transition';
+
+export default function Template({ children }: { children: ReactNode }) {
+  return <PageTransition>{children}</PageTransition>;
+}
