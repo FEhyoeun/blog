7bf49f6b4522ff43a6973189eb6700b85aa170ed
FEhyoeun <khyoeun92@gmail.com>
2025-08-10 22:29:58 +0900

    fix: createSupabaseClient 함수 수정 * url, key가 없을 경우 에러 추가

--- packages/shared/src/supabaseClient.ts
+++ packages/shared/src/supabaseClient.ts
@@ -1,5 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const createSupabaseClient = (url: string, key: string) => {
+export const createSupabaseClient = (url?: string, key?: string) => {
+  if (!url || !key) {
+    throw new Error("Supabase URL and key are required");
+  }
   return createClient(url, key);
 };
