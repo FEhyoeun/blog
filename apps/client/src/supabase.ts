bf443df6876bc75d1fde186f758acd4e5c96f74d
FEhyoeun <khyoeun92@gmail.com>
2025-08-10 22:33:25 +0900

    fix: getSupabaseClient 함수 수정 * 서버 사이드 렌더링 중 supabaseClient가 환경변수 못 찾음 -> 404 에러 발생. -> lazy initialization

--- apps/client/src/supabase.ts
+++ apps/client/src/supabase.ts
@@ -1,8 +1,15 @@
 import { createSupabaseClient } from '@shared/shared/src/supabaseClient';
 
-const supabaseClient = createSupabaseClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
+let supabaseClient: ReturnType<typeof createSupabaseClient> | null = null;
 
-export default supabaseClient;
+export const getSupabaseClient = () => {
+  if (!supabaseClient) {
+    supabaseClient = createSupabaseClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    );
+  }
+  return supabaseClient;
+};
+
+export default getSupabaseClient();
